3d40f480de5462224744d5ba2cc2080be907be4b
Vinil Prabhu <vinilprabhu@gmail.com>
2021-08-04 23:23:56 +0530

    changed app name and package

--- android/app/src/debug/java/com/vinil/utility/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/vinil/utility/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.utility;
+package com.vinil.utility;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
